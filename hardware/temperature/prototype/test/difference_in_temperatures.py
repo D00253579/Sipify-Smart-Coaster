87abd903b7f8d0415eb3368184198c00a6ac7294
LukeHilliard <119594459+LukeHilliard@users.noreply.github.com>
2024-11-18 20:51:49 +0000

    Developed a temperature test
    This test uses a contact sensor placed on the surface of the
    cardboard prototype and a submersible sensor placed inside a
    coffee cup. The output is written to a file.
    
    We plan to use this data to better understand how and where
    temperature increases on a cup

---
+++ hardware/temperature/prototype/test/difference_in_temperatures.py
@@ -0,0 +1,44 @@
+import os
+import glob
+import time
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+
+# setup contact sensor
+contact_folder = glob.glob(base_dir + '28*')[0]
+contact_file = contact_folder + '/w1_slave'
+
+# setup submergible sensor
+submergible_folder = glob.glob(base_dir + '28*')[1]
+submergible_file = submergible_folder + '/w1_slave'
+
+def write_to_file(contact_temp, submergible_temp):
+    with open('temperature_difference_test.txt', 'a') as file:
+        file.write(f"Contact Temp: {contact_temp} | Submergible Temp: {submergible_temp} | Timestamp: {time.strftime('%H:%M:%S')}\n")
+
+
+def read_temp_raw(file):
+    f = open(file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp(file):
+    lines = read_temp_raw(file)
+    temp_data = lines[1]
+    temperature = temp_data.split('=')[1][:2]	
+    return temperature
+
+while True:
+
+    contact_temp = read_temp(contact_file)
+    submergible_temp = read_temp(submergible_file)
+
+    print(f"Contact Temp: {contact_temp}")
+    print(f"Submergible Temp: {submergible_temp}")
+
+    write_to_file(contact_temp, submergible_temp)
+    time.sleep(1) 
